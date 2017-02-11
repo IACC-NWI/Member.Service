8d21028804226be69d16d9c16f5afb6fcdf3ff03
madhavabhyankar <madhavsemail@gmail.com>
2017-02-11 14:42:50 -0600

    Member Service Shell Created

---
+++ Member.Service/Database/MemberDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Member.Service.Database
+{
+    public class MemberDbContext: DbContext, IMemberDbContext
+    {
+        public MemberDbContext()
+            : base("memberDbConnectionString")
+        {
+            
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<DecimalPropertyConvention>();
+            modelBuilder.Conventions.Add(new DecimalPropertyConvention(14, 2));
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Member.Service/IoC/Installer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using Member.Service.Database;
+
+namespace Member.Service.IoC
+{
+    public class Installer : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(
+                Component.For<IMemberDbContext>().ImplementedBy<MemberDbContext>().LifestyleTransient(),
+                Classes.FromThisAssembly().BasedOn<ApiController>().LifestyleTransient());
+        }
+    }
+}
